858e38c494bbc79903f188119549ab6bb703e0d7
Walter Nam <49884140+wnam98@users.noreply.github.com>
2020-07-02 18:25:50 +0900

    added lowest common ancestor BST solution.

--- main.cpp
+++ main.cpp
@@ -186,6 +186,13 @@ public:
         return helper(root, 0);
     }
 
+    //lowest common ancestor BST
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q){
+        if(p -> val < root -> val && q -> val < root -> val) return lowestCommonAncestor(root -> left, p, q);
+        if(p -> val > root -> val && q -> val > root -> val) return lowestCommonAncestor(root -> right, p, q);
+        return root;
+    }
+
 //End of tree questions
 
 //Reverse linkedlist
