d7957fc9231dfdfd580467b4e147b9bfe45b6e3b
Walter Nam <49884140+wnam98@users.noreply.github.com>
2020-07-09 20:38:21 +0900

    added maximum width of binary tree solution

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <list>
 #include <map>
 #include <set>
+#include <queue>
 
 using namespace std;
 
@@ -430,6 +431,34 @@ public:
             }
         }
         return res;
+    }
+
+    int widthOfBinaryTree(TreeNode* root) {
+        int ans = 0;
+
+        queue<pair<TreeNode*, unsigned long long>> q;
+        if (root)
+            q.push({root, 1});
+
+        while (!q.empty()) {
+            int cnt = q.size();
+            unsigned long long left = q.front().second, right;
+            for (int i = 0; i < cnt; i++) {
+                TreeNode* n = q.front().first;
+                right = q.front().second;
+                q.pop();
+                if (n->left != nullptr) {
+                    q.push({n->left, 2*right});
+                }
+                if (n->right != nullptr) {
+                    q.push({n->right, 2*right+1});
+                }
+            }
+            //cout << right << " " << left << "\n";
+            ans = max(ans, (int)(right - left + 1));
+        }
+
+        return ans;
     }
 };
 
