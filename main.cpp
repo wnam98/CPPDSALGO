4588830188a06ee7074fceed983afa0bb1c6361d
Walter Nam <49884140+wnam98@users.noreply.github.com>
2020-07-12 19:42:11 +0900

    added reverse bits problem leetcode july challenge

--- main.cpp
+++ main.cpp
@@ -506,6 +506,17 @@ public:
         }
         return subset;
     }
+
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t res = 0;
+        int b = 32;
+        while(b--){
+            res <<= 1;
+            res += n % 2;
+            n >>= 1;
+        }
+        return res;
+    }
 };
 
 class graph {
