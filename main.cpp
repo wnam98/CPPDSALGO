c7ac3a2be4a596fcae1a21b690ae568570153804
Walter Nam <49884140+wnam98@users.noreply.github.com>
2020-07-13 22:58:23 +0900

    added sametree leetcode july challenge

--- main.cpp
+++ main.cpp
@@ -517,6 +517,13 @@ public:
         }
         return res;
     }
+
+     bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p == NULL && q == NULL) return true;
+        if(p == NULL || q == NULL) return false;
+        if(p->val == q->val) return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+        return false;
+    }
 };
 
 class graph {
