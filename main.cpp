dcc8080d4819c1110b64f05134a68dcc61c9d044
Walter Nam <49884140+wnam98@users.noreply.github.com>
2020-07-15 16:03:00 +0900

    added reverse words in a string leetcode july challenge

--- main.cpp
+++ main.cpp
@@ -540,6 +540,28 @@ public:
 
         return angle;
     }
+
+    string reverseWords(string s) {
+        reverse(s.begin(), s.end());
+
+        //reverse words
+        for(int i = 0, j = 0;i<s.size();){
+            while(i<s.size() && (i < j || s[i]==' ') ) i++;
+            while(j<s.size() && (j<i || s[j]!=' ')) j++;
+            reverse(s.begin()+i, s.begin()+j);
+        }
+
+        //remove space
+        int i = 0,j = 0;
+        while(j<s.size()){
+            if(s[j]!=' ' || (s[j]==' ' && i!=0 && j!= s.size()-1 && s[j+1]!= ' '))
+                s[i++] = s[j++];
+            else if(s[j]==' ')
+                j++;
+        }
+        s.resize(i);
+        return s;
+    }
 };
 
 class graph {
