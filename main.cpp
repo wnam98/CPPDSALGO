ade76f9e174f9d3f96595dee9d9f4edaea5fb14f
Walter Nam <49884140+wnam98@users.noreply.github.com>
2020-07-26 17:20:19 +0900

    added add digits leetcode july challenge

--- main.cpp
+++ main.cpp
@@ -706,6 +706,13 @@ public:
         sort(nums.begin(), nums.end());
         return nums.at(0);
     }
+
+    int addDigits(int num) {
+        int mod = num%9;
+        if(num==0) return 0;
+        if(mod==0) return 9;
+        return mod;
+    }
 };
 
 class graph {
