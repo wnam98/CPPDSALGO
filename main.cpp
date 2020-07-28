4772a3d95088db3d65c865df287ce9749a048565
Walter Nam <49884140+wnam98@users.noreply.github.com>
2020-07-28 20:42:00 +0900

    added task scheduler july leetcode challenge

--- main.cpp
+++ main.cpp
@@ -734,6 +734,37 @@ public:
         return create(inorder,postorder,0,inorder.size()-1,0,postorder.size()-1);
     }
 
+public:
+    int leastInterval(vector<char>& t, int n) {
+        map <char,int> m;
+        for(int i =0;i<t.size();i++){
+            m[t[i]]++;
+        }
+        map <char, int> :: iterator i = m.begin();
+        priority_queue <int> pq;
+        while(i != m.end()){
+            pq.push(i->second);
+            i++;
+        }
+        int ans = 0;
+        int cycle = n + 1;
+        while(!pq.empty()){
+            vector <int> temp;
+            int time = 0;
+            for(int i = 0; !pq.empty() && i < cycle; i++){
+                temp.push_back(pq.top());
+                pq.pop();
+                time++;
+            }
+            for(int i = 0;i < temp.size(); i++){
+                temp[i]-- ;
+                if(temp[i])pq.push(temp[i]);
+            }
+            ans += pq.empty()? time : cycle;
+        }
+        return ans;
+    }
+
 };
 
 class graph {
