d7ecd778d7ce8f0ae75cd98ae7cea102db7d4748
Walter Nam <49884140+wnam98@users.noreply.github.com>
2020-08-11 23:23:13 +0900

    added hindex august leetcode challenge

--- main.cpp
+++ main.cpp
@@ -944,6 +944,31 @@ public:
 
         return result;
     }
+
+public:
+    int hIndex(vector<int>& citations) {
+        int N = citations.size();
+        vector<int> buckets(N+1, 0);
+
+        for(int i : citations){
+            if (i >= N){
+                buckets[N]++;
+            } else {
+                buckets[i]++;
+            }
+        }
+
+        int count = 0;
+        for(int i = N; i >= 0; i--){
+            count += buckets[i];
+            if(count >= i) {
+                return i;
+            }
+        }
+
+        // should not reach here
+        return 0;
+    }
 };
 
 class MyHashSet {
