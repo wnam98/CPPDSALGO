63a3195f969adc0563f50c0b2c32173570422161
Walter Nam <49884140+wnam98@users.noreply.github.com>
2020-08-14 17:44:29 +0900

    added longestPalindrome August leetcode challenge

--- main.cpp
+++ main.cpp
@@ -1026,6 +1026,18 @@ public:
     bool contains(int key){
         return contains(key, GetHash(key));
     }
+
+public:
+    int longestPalindrome(string s) {
+        vector<int>v(255,0);
+        int ans=0;
+        for(int i=0;i<s.size();i++)
+        {
+            v[s[i]-'A']++;
+            if(v[s[i]-'A'] % 2 == 0) ans+=2;
+        }
+        return s.size() > ans ? ans + 1 : ans;
+    }
 };
 
 class graph {
