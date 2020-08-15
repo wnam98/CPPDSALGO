9581bb325b45351dafba0ef6e2d96eeb966618d0
Walter Nam <49884140+wnam98@users.noreply.github.com>
2020-08-15 16:37:01 +0900

    added non-overlapping intervals august leetcode solution

--- main.cpp
+++ main.cpp
@@ -1038,6 +1038,25 @@ public:
         }
         return s.size() > ans ? ans + 1 : ans;
     }
+public:
+    static bool cmp(vector <int>& a, vector <int>& b){
+        return a[1] < b[1];
+    }
+    int eraseOverlapIntervals(vector<vector<int>>& arr) {
+        int n = arr.size();
+        if(!n)return 0;
+        int cnt = 1;
+        sort(arr.begin(), arr.end(), cmp);
+        int end = arr[0][1];
+        for(int i = 1; i < n; i++){
+            if(arr[i][0] >= end){
+                end = arr[i][1];
+                cnt++;
+            }
+        }
+        return n - cnt;
+    }
+
 };
 
 class graph {
