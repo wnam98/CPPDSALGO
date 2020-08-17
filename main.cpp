8a1dd922ac356bf9aa5d254b12db579f6700d343
Walter Nam <49884140+wnam98@users.noreply.github.com>
2020-08-17 17:16:35 +0900

    added distribute candies august leetcode challenge

--- main.cpp
+++ main.cpp
@@ -1057,6 +1057,23 @@ public:
         return n - cnt;
     }
 
+public:
+    static vector<int> distributeCandies(int candies, int num_people) {
+        vector<int> ans(num_people, 0);
+        int i = 0;
+        while (candies > 0) {
+            ans[i % num_people] += i + 1;
+            candies -= i + 1;
+            ++i;
+        }
+
+        if (candies < 0) { //Fix for giving too many candies to last person
+            ans[(i - 1) % num_people] += candies;
+        }
+
+        return ans;
+    }
+
 };
 
 class graph {
