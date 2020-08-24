fc037576c750ec0ab1c22c3ea07f1db8e260276a
Walter Nam <49884140+wnam98@users.noreply.github.com>
2020-08-24 16:56:45 +0900

    sum of left leaves august leetcode challenge

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/aircr/OneDrive/Desktop/College/CPPDSALGO
+# Install script for directory: C:/Users/aircr/OneDrive/Desktop/College/C++ Code/CPPDSALGO
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/aircr/OneDrive/Desktop/College/CPPDSALGO/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/aircr/OneDrive/Desktop/College/C++ Code/CPPDSALGO/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- main.cpp
+++ main.cpp
@@ -1150,6 +1150,12 @@ public:
         }
     }
 
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        if(root == NULL) return 0;
+        if(root -> left != NULL && root -> left -> left == NULL && root -> left -> right == NULL)  return root -> left -> val + sumOfLeftLeaves(root -> right);
+        return sumOfLeftLeaves(root -> left) + sumOfLeftLeaves(root -> right);
+    }
 };
 
 class graph {
