b8d12f6784cddb664d9f182a64369bf064867c12
Walter Nam <49884140+wnam98@users.noreply.github.com>
2020-08-26 19:43:50 +0900

    fizzbuzz august leetcode challenge

--- main.cpp
+++ main.cpp
@@ -1156,6 +1156,23 @@ public:
         if(root -> left != NULL && root -> left -> left == NULL && root -> left -> right == NULL)  return root -> left -> val + sumOfLeftLeaves(root -> right);
         return sumOfLeftLeaves(root -> left) + sumOfLeftLeaves(root -> right);
     }
+
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> res;
+        for(int i=1; i<=n; i++)
+        {
+            if(i%3 == 0 && i%5 == 0)
+                res.push_back("FizzBuzz");
+            else if(i%3 == 0)
+                res.push_back("Fizz");
+            else if(i%5 == 0)
+                res.push_back("Buzz");
+            else
+                res.push_back(to_string(i));
+        }
+        return res;
+    }
 };
 
 class graph {
